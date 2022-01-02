d09d39330767efc629a2124909f1a880227b62ea
mnmxmx <nknmsm1992@gmail.com>
2022-01-02 19:58:57 +0900

    added gui

--- js/modules/Artwork.js
+++ js/modules/Artwork.js
@@ -7,6 +7,8 @@ import * as THREE from "three"
 
 import bokeh_vert from "./glsl/bokeh.vert";
 import bokeh_frag from "./glsl/bokeh.frag";
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
 
 export default class Artwork{
     constructor(props){
@@ -27,11 +29,16 @@ export default class Artwork{
 
         this.scene = new THREE.Scene();
         this.camera = new THREE.Camera();
+        
 
         this.fbos = [
             new THREE.WebGLRenderTarget(common.fbo_dimensions.x, common.fbo_dimensions.y),
-            // new THREE.WebGLRenderTarget(common.fbo_dimensions.x, common.fbo_dimensions.y),
-            // new THREE.WebGLRenderTarget(common.fbo_dimensions.x, common.fbo_dimensions.y),
+            new THREE.WebGLRenderTarget(common.fbo_dimensions.x, common.fbo_dimensions.y),
+            new THREE.WebGLRenderTarget(common.fbo_dimensions.x, common.fbo_dimensions.y),
+            new THREE.WebGLRenderTarget(common.fbo_dimensions.x, common.fbo_dimensions.y),
+            new THREE.WebGLRenderTarget(common.fbo_dimensions.x, common.fbo_dimensions.y),
+            new THREE.WebGLRenderTarget(common.fbo_dimensions.x, common.fbo_dimensions.y),
+            new THREE.WebGLRenderTarget(common.fbo_dimensions.x, common.fbo_dimensions.y),
         ];
 
         this.plane = new THREE.Mesh(
@@ -51,8 +58,12 @@ export default class Artwork{
                     },
                     uDirection: {
                         value: 1
+                    },
+                    uBlur: {
+                        value: true
                     }
-                }
+                },
+                flatShading: true
             })
         );
 
@@ -63,6 +74,20 @@ export default class Artwork{
         });
 
         assets.load();
+
+        this.guiParams = {
+            blur_iteration: 1,
+            isBlur: true
+        }
+
+        this.gui = new GUI();
+        document.body.appendChild(this.gui.domElement);
+
+        this.gui.add(this.guiParams, "blur_iteration", 1, 8, 1);
+        this.gui.add(this.guiParams, "isBlur")
+
+        this.stats = new Stats();
+        document.body.appendChild(this.stats.domElement);
 
         this.loop();
     }
@@ -81,35 +106,51 @@ export default class Artwork{
     }
 
     update(){
+        this.stats.begin();
         common.update();
         this.mainScene.update();
 
         const uniforms = this.plane.material.uniforms
 
-        uniforms.uOutput.value = false;
-
-        for(let i = 0; i <= this.fbos.length; i++){
-
-            if(i == 0){
-                uniforms.uDiffuse.value = this.mainScene.fbo.texture
-            } else {
-                uniforms.uDiffuse.value = this.fbos[i - 1].texture
-
+        if(this.guiParams.isBlur){
+            uniforms.uOutput.value = false;
+            uniforms.uBlur.value = true;
+        
+            for(let i = 0; i <= this.guiParams.blur_iteration - 1; i++){
+    
+                if(i == 0){
+                    uniforms.uDiffuse.value = this.mainScene.fbo.texture
+                } else {
+                    uniforms.uDiffuse.value = this.fbos[i - 1].texture
+                }
+    
+                uniforms.uDirection.value = i % 2 == 0 ? 1 : -1;
+    
+                if(i < this.guiParams.blur_iteration - 1){
+                    common.renderer.setRenderTarget(this.fbos[i]);
+                    common.renderer.render(this.scene, this.camera);
+                } else {
+                    uniforms.uOutput.value = true;
+    
+                    common.renderer.setRenderTarget(null);
+                    common.renderer.render(this.scene, this.camera);
+                }
+                
             }
+        } else {
 
-            uniforms.uDirection.value = i % 2 == 0 ? 1 : -1;
+            uniforms.uDiffuse.value = this.mainScene.fbo.texture
+            uniforms.uOutput.value = true;
+            uniforms.uBlur.value = false;
 
-            if(i < this.fbos.length){
-                common.renderer.setRenderTarget(this.fbos[i]);
-                common.renderer.render(this.scene, this.camera);
-            } else {
-                uniforms.uOutput.value = true;
-
-                common.renderer.setRenderTarget(null);
-                common.renderer.render(this.scene, this.camera);
-            }
-            
+            common.renderer.setRenderTarget(null);
+            common.renderer.render(this.scene, this.camera);
         }
+
+        
+
+        this.stats.end();
+
 
     }
 
--- js/modules/glsl/bokeh.frag
+++ js/modules/glsl/bokeh.frag
@@ -2,46 +2,46 @@ uniform sampler2D uDiffuse;
 uniform vec2 uResolution;
 uniform bool uOutput;
 uniform float uDirection;
+uniform bool uBlur;
 
 varying vec2 vUv;
+
+float factor;
 
 // use glsl-fast-gaussian-blur
 // https://github.com/Jam3/glsl-fast-gaussian-blur
 
-vec4 blur13(sampler2D image, vec2 uv, vec2 resolution, vec2 direction, float factor) {
-  vec4 color = vec4(0.0);
-  vec2 off1 = vec2(1.411764705882353) * direction * factor;
-  vec2 off2 = vec2(3.2941176470588234) * direction * factor;
-  vec2 off3 = vec2(5.176470588235294) * direction * factor;
-  color += texture2D(image, uv + (off1 / resolution)) * 0.2969069646728344;
-  color += texture2D(image, uv - (off1 / resolution)) * 0.2969069646728344;
-  color += texture2D(image, uv + (off2 / resolution)) * 0.09447039785044732;
-  color += texture2D(image, uv - (off2 / resolution)) * 0.09447039785044732;
-  color += texture2D(image, uv + (off3 / resolution)) * 0.010381362401148057;
-  color += texture2D(image, uv - (off3 / resolution)) * 0.010381362401148057;
-  return color;
-}
+// change blur weight depends according to fogFactor
+vec3 blur9(sampler2D image, vec2 uv, vec2 resolution, vec2 direction) {
+  vec3 color = vec3(0.0);
+  vec2 off1 = vec2(1.3846153846) * direction;
+  vec2 off2 = vec2(3.2307692308) * direction;
 
-vec4 blur9(sampler2D image, vec2 uv, vec2 resolution, vec2 direction, float factor) {
-  vec4 color = vec4(0.0);
-  vec2 off1 = vec2(1.3846153846) * direction * factor;
-  vec2 off2 = vec2(3.2307692308) * direction * factor;
-//   color += texture2D(image, uv) * 0.2270270270;
-  color += texture2D(image, uv + (off1 / resolution)) * 0.3162162162;
-  color += texture2D(image, uv - (off1 / resolution)) * 0.3162162162;
-  color += texture2D(image, uv + (off2 / resolution)) * 0.0702702703;
-  color += texture2D(image, uv - (off2 / resolution)) * 0.0702702703;
-  return color;
-}
+  vec4 color0 = texture2D(image, vUv);
+  vec4 color1 = texture2D(image, uv + (off1 / resolution));
+  vec4 color2 = texture2D(image, uv - (off1 / resolution));
+  vec4 color3 = texture2D(image, uv + (off2 / resolution));
+  vec4 color4 = texture2D(image, uv - (off2 / resolution));
 
+  factor = color0.a;
+
+  float weight1 = color1.a * factor * 0.3162162162;
+  float weight2 = color2.a * factor * 0.3162162162;
+  float weight3 = color3.a * factor * 0.0702702703;
+  float weight4 = color4.a * factor* 0.0702702703;
+
+  color += color1.rgb * weight1;
+  color += color2.rgb * weight2;
+  color += color3.rgb * weight3;
+  color += color4.rgb * weight4;
+
+  color += color0.rgb * (1.0 - (weight1 + weight2 + weight3 + weight4));
+  return uBlur ? color : color0.rgb;
+}
 
 void main(){
-    vec4 diffuse = texture2D(uDiffuse, vUv);
+    vec3 color = vec3(0.0);
+    color += blur9(uDiffuse, vUv, uResolution, vec2(1.0, 1.0 * uDirection));
 
-    float factor = diffuse.a;
-
-    diffuse *= 0.1964825501511404; //0.2270270270;//
-    diffuse += blur13(uDiffuse, vUv, uResolution, vec2(1.0, 1.0 * uDirection), factor);
-
-    gl_FragColor = vec4(diffuse.rgb, uOutput ? 1.0 : factor);
+    gl_FragColor = vec4(color, uOutput ? 1.0 : factor);
 }
